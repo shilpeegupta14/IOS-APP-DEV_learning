7fd72416f562d62a4038aafb383f09deda3a8d1c
SHILPEE GUPTA <78029920+shilpeegupta14@users.noreply.github.com>
2021-08-15 07:28:33 -0700

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Shilpee gupta on 12/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
